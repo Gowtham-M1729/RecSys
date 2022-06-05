d5bdbdc7ac83ae39e1878cae1e52a415bcf3707a
Gowtham-M1729 <gowtham1842001@gmai.com>
2022-06-05 17:23:33 +0530

    Add my project

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI,Response, status
+from fastapi.responses import JSONResponse
+from recomsystem import get_recommendations_new
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id:int):
+    return {"item_id": item_id}
+
+@app.get("/recommend/{show_name}",status_code=200)
+async def read_item(show_name:str,):
+    try:
+        pdsr=get_recommendations_new(show_name)
+    except:
+        Response.status_code=status.HTTP_403_FORBIDDEN
+        return {"message":"Movie Not Found in Database"}
+    lst=list(pdsr.values)
+    print(lst)
+    return JSONResponse(content=lst,media_type="application/json")
