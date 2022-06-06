4291e4211a7b2ed1596476dcf52ff0cc6944eb80
Gowtham M <55031924+Gowtham-M1729@users.noreply.github.com>
2022-06-06 07:56:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI,Response, status
 from fastapi.responses import JSONResponse
 # from recomm.recomsystem import get_recommendations_new
 from recomm import recomsystem as rec
+from recomm import disney_recomm as dsrec
 application = FastAPI()
 app=application
 @app.get("/")
@@ -16,6 +17,19 @@ async def read_item(item_id:int):
 async def read_item(show_name:str,):
     try:
         pdsr=rec.get_recommendations_new(show_name)
+    except:
+        Response.status_code=status.HTTP_403_FORBIDDEN
+        return {"message":"Movie Not Found in Database"}
+    lst=list(pdsr.values)
+    print(lst)
+    return JSONResponse(content=lst,media_type="application/json")
+
+
+
+@app.get("/disney_recommend/{show_name}",status_code=200)
+async def read_item(show_name:str):
+    try:
+        pdsr=dsrec.extended_recommender(show_name)
     except:
         Response.status_code=status.HTTP_403_FORBIDDEN
         return {"message":"Movie Not Found in Database"}
